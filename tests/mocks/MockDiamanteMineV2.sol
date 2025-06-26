ac1729840b8fa7d7fc98bf63fdc79b7ab3fd6c9f
Brian Le <brian@brianle.xyz>
2025-06-26 12:31:18 -0400

    feat: add DiamanteMineV2 mock for upgrades
    This commit adds a `MockDiamanteMineV2` contract to simulate contract upgrades.
    
    - Adds `MockDiamanteMineV2.sol` with a `version()` function.
    - Updates `DiamanteMine.t.sol` to include upgrade tests.

--- tests/mocks/MockDiamanteMineV2.sol
+++ tests/mocks/MockDiamanteMineV2.sol
@@ -2,8 +2,14 @@
 pragma solidity ^0.8.30;
 
 import { DiamanteMineV1 } from "../../src/DiamanteMineV1.sol";
+import { ISignatureTransfer } from "@uniswap/permit2/src/interfaces/ISignatureTransfer.sol";
 
 contract MockDiamanteMineV2 is DiamanteMineV1 {
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor(ISignatureTransfer _permit2) DiamanteMineV1(_permit2) {
+        _disableInitializers();
+    }
+
     function version() public pure override returns (string memory) {
         return "2.0.0";
     }
